1228fe3b62178ef954d4b58e0370bde3738ef71a
Gaurav Tiwari <144383446+CodeBy-Gaurav@users.noreply.github.com>
2023-12-05 23:04:29 +0530

    Add files via upload

---
+++ JS/Basics/04_comparition.js
@@ -0,0 +1,23 @@
+console.log(2 > 2)
+console.log(2 >= 2)
+console.log(2 == 2)
+
+console.log("4"> 2) // => true
+console.log("2"== 2)// => true 
+
+//Javascript first change the datatype of string to number then compare
+
+
+ console.log(null > 0 )// => false
+ console.log(null == 0 )// => false
+ //changes the value of null to '0' then compare
+
+ console.log(undefined > 0) // output => false
+ console.log(undefined == 0)// output => false
+ //undefined cannot be converted to an number
+
+console.log("2" === 2) //=> false
+ // === "Strict check "
+ //it also checks the datatype of variable
+ 
+
